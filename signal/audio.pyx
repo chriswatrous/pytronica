7be9cc91cc2866c2c733d2c98e58bbbe782b57da
Chris Watrous <chriswatrous@gmail.com>
2015-04-21 03:47:05 -0500

    add Compose class

--- signal/audio.pyx
+++ signal/audio.pyx
@@ -3,6 +3,8 @@ from libc.stdio cimport putchar, fflush, stdout
 from libc.string cimport memset
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
 
+from collections import deque
+
 cdef int buffer_size = 1024
 cdef double sample_rate = 48000
 
@@ -19,8 +21,8 @@ cdef class Signal:
 
         while True:
             length = self.generate()
+
             if length == 0:
-                fflush(stdout)
                 return
 
             for i in range(length):
@@ -184,3 +186,120 @@ cdef class DEnv(BufferSignal):
             self.value *= self.step
 
         return buffer_size
+
+
+cdef class ComposeInfo:
+    cdef Signal signal
+    cdef int start
+    cdef int offset
+    cdef int length
+
+    def __cinit__(self, Signal signal, int start, int offset):
+        self.signal = signal
+        self.start = start
+        self.offset = offset
+        self.length = 0
+
+
+cdef class Compose(BufferSignal):
+    cdef object inputs, waiting, running
+    cdef int generate_count
+    cdef bint started
+
+    def __init__(self, inputs=None):
+        if inputs:
+            self.inputs = inputs
+        else:
+            self.inputs = []
+
+        self.started = False
+        self.generate_count = 0
+
+    def add(self, signal, delay):
+        self.inputs.append((signal, delay))
+
+    cdef _prepare(self):
+        cdef int start, offset
+        cdef Signal signal
+        cdef double delay
+
+        self.started = True
+
+        self.inputs.sort(key=lambda x: x[1])
+
+        self.waiting = deque()
+        self.running = []
+
+        for signal, delay in self.inputs:
+            start = <int>((delay * sample_rate) / buffer_size)
+            offset = <int>((delay * sample_rate) - (start * buffer_size))
+            self.waiting.append(ComposeInfo(signal, start, offset))
+
+
+    cdef int generate(self) except -1:
+        cdef ComposeInfo info
+        cdef int i # used for loop iterators
+        cdef int length # used for the loop terminator
+        cdef int total_length # the total number of samples in the buffer that were touch by any loop
+
+        total_length = 0
+
+        if not self.started:
+            self._prepare()
+
+        # End this signal if there are no more child signals to be played.
+        if len(self.running) == 0 and len(self.waiting) == 0:
+            return 0
+
+        # Zero out the buffer.
+        memset(self.samples, 0, buffer_size * sizeof(double))
+
+        # Get the signals that will be starting this round.
+        starting = set()
+        while True:
+            if len(self.waiting) == 0:
+                break
+            info = self.waiting[0]
+            if info.start > self.generate_count:
+                break
+            x = self.waiting.popleft()
+            starting.add(x)
+            self.running.append(x)
+
+        done = []
+        for info in self.running:
+            # Add the end of the previously generated signal to the start of the buffer.
+            if info not in starting:
+                length = info.length + info.offset - buffer_size
+                for i in range(length):
+                    self.samples[i] += info.signal.samples[i + buffer_size - info.offset]
+                total_length = imax(length, total_length)
+
+            # Generate the signal.
+            info.length = info.signal.generate()
+            if info.length == 0:
+                done.append(info)
+                continue
+
+            # Add the start of the generated signal to the end of the buffer.
+            length = imin(info.length + info.offset, buffer_size)
+            for i in range(info.offset, length):
+                self.samples[i] += info.signal.samples[i - info.offset]
+            total_length = imax(length, total_length)
+
+        for x in done:
+            self.running.remove(x)
+
+        self.generate_count += 1
+
+        if len(self.running) == 0 and len(self.waiting) == 0:
+            return total_length
+        else:
+            return buffer_size
+
+
+cdef inline int imax(int a, int b):
+    return a if a > b else b
+
+cdef inline int imin(int a, int b):
+    return a if a < b else b
--- signal/song.py
+++ signal/song.py
@@ -4,12 +4,11 @@ pyximport.install()
 
 import re
 
-from audio import Saw, Mul, Layer, AmpMod, DEnv
+from audio import Saw, Mul, Layer, AmpMod, DEnv, Compose
 
 
 def p2f(p):
     return 440 * 2**((p - 69)/12.0)
-
 
 note_names = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
 note_modifiers = {None: 0, 'b': -1, '#': 1}
@@ -28,16 +27,36 @@ def notes(s):
 def note_freq(s):
     return p2f(note(s))
 
-
-ss = [Saw(p2f(x)) for x in [43, 57, 58, 62, 65]]
 def play_notes(s):
     ss = [Saw(p2f(x)) for x in notes(s)]
-    Mul(AmpMod(Layer(ss), DEnv(0.5)), 0.25).play()
+    return AmpMod(Layer(ss), DEnv(0.4))
 
-play_notes('Ab2 Bb3 C4 Eb4 G4')
-play_notes('Gb2 Ab3 Bb3 Db4 F4')
-play_notes('A2 B3 C#4 E4 G#4')
-play_notes('B2 Db4 Eb4 Gb4 Bb4')
+#c = Compose()
+#delay = 0
+#step = 1.5
+#for s in ['Ab2 Bb3 C4 Eb4 G4', 'Gb2 Ab3 Bb3 Db4 F4', 'A2 B3 C#4 E4 G#4', 'B2 Db4 Eb4 Gb4 Bb4']*4:
+    #c.add(play_notes(s), delay)
+    #delay += step
 
 #a = AmpMod(Saw(note_freq('Ab4')), DEnv(1))
 #a.play()
+
+adj = 0.1
+def osc(p):
+    ss = [Saw(p2f(p+adj*x)) for x in range(-2, 3)]
+    return Layer(ss)
+
+def synth(p):
+    return AmpMod(osc(p), DEnv(0.5))
+
+c = Compose()
+delay = 0
+step = 0.2
+
+for p in (notes('Ab3 C4 Eb4 G4') * 4 + notes('Eb3 G3 Bb3 D4') * 4)*4:
+    c.add(synth(p), delay)
+    delay += step
+
+
+m = Mul(c, 0.15)
+m.play()
