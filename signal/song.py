d61eb77cb4de97c18ef6c6a20eb1ba8828d73cf1
Chris Watrous <chriswatrous@gmail.com>
2015-04-22 00:16:18 -0500

    update test song

--- signal/song.py
+++ signal/song.py
@@ -43,20 +43,62 @@ def play_notes(s):
 
 adj = 0.1
 def osc(p):
-    ss = [Saw(p2f(p+adj*x)) for x in range(-2, 3)]
+    ss = [Saw(p2f(p+adj*x)) for x in range(-1, 2)]
     return Layer(ss)
 
 def synth(p):
-    return AmpMod(osc(p), DEnv(0.5))
+    return AmpMod(osc(p), DEnv(0.3))
 
 c = Compose()
 delay = 0
-step = 0.2
+step = 0.18
 
-for p in (notes('Ab3 C4 Eb4 G4') * 4 + notes('Eb3 G3 Bb3 D4') * 4)*4:
+#for p in (notes('Ab3 C4 Eb4 G4') * 4 + notes('Eb3 G3 Bb3 D4') * 4)*4:
+for p in notes('G3 C4 E4 C4 B4 E4 C4  A3 D4 F#4 D4 C5 F#4 D4') * 8:
     c.add(synth(p), delay)
     delay += step
 
+def synth2(p):
+    return AmpMod(Layer([osc(p), osc(p+7)]), DEnv(.5))
 
-m = Mul(c, 0.15)
+c2 = Compose()
+delay = 0
+for p in notes('C2 D2 A1 D2'):
+    c2.add(synth2(p), delay)
+    delay += step * 14
+
+for p in notes('C2 D2 A1'):
+    c2.add(synth2(p), delay)
+    c2.add(synth2(p), delay + step*3)
+    c2.add(synth2(p), delay + step*7)
+    c2.add(synth2(p), delay + step*10)
+    delay += step * 14
+
+
+c2.add(synth2(note('D2')), delay)
+delay += step*3
+c2.add(synth2(note('D2')), delay)
+delay += step*4
+c2.add(synth2(note('D2')), delay)
+delay += step*4
+c2.add(synth2(note('C2')), delay)
+delay += step*1
+c2.add(synth2(note('D2')), delay)
+delay += step*2
+c2.add(synth2(note('C2')), delay)
+
+def part3():
+    def synth(p):
+        return AmpMod(osc(p), DEnv(0.1))
+    c = Compose()
+    ps = notes('F#3 G3 A3 C4 D4 F#4 G4 A4 C5 D5 F#5')
+    ps = [ps[x] for x in [0, 1, 2, 3, 4, 3, 2, 3, 4, 5, 6, 5, 4, 5, 6, 7, 8, 7, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
+    delay = step*(14*4 + 4)
+    for p in ps:
+        c.add(synth(p), delay)
+        delay += step / 2
+    return c
+
+m = Mul(Layer([c, c2, part3()]), 0.15)
+#m = Mul(part3(), 0.15)
 m.play()
