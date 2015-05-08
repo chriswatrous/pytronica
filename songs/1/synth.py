09cdeb28ac5a2eabacb3ed23c89806ea529fe39a
Chris Watrous <chriswatrous@gmail.com>
2015-05-07 21:15:31 -0500

    fix a wrong rename

--- songs/1/synth.py
+++ songs/1/synth.py
@@ -1,97 +1,47 @@
 #! /usr/bin/python
 from __future__ import division
+from random import random
 
-from time import time
-import re
-from itertools import islice
+if __name__ == '__main__':
+    import sys
+    sys.path.append('../../lib')
 
-import sys
-sys.path.append('../../lib')
 from audio import *
 
-from song1_synth import *
-
-pitch_spread = 0.1
-
-step = .18
-
-
-def f(ss):
-    return 0.5 * repeat(arp1s(ss), 1000)
-
-part1 = f(['G3 C4 E4 B4', 'A3 D4 F#4 C5'])
-part1a = part1 + f(['E3 A3 C4 G4', 'F#3 B3 D4 A4'])
-part1a.playx()
-
-def osc(p):
-    return psaw(p + pitch_spread).pan(-.5) + psaw(p - pitch_spread).pan(.5)
-
-def synth(p, d):
-    return (osc(p) + osc(p+7)) * ExpDecay(d)
-
-def f(d):
-    a = Chain()
-    for p in notes('C2 D2 A1 D2'):
-        a.add(synth(p, d), step * 14)
-    return a
-
-a = f(.75)
-b = f(.5)
-
-c = Chain()
-for x in [3, 4, 3, 4]:
-    c.add(b, step * x)
-c.mlength = b.mlength
-
-part2 = 0.25 * Chain([a, c])
+pitch_spread = 0.2
+def synth1(p, pn, random_phase=0):
+    osc = Layer()
+    n = 10
+    d = 4
+    for x in f_range(-pitch_spread, pitch_spread, n):
+        osc.add(psaw(p + x, phase=random_phase*random()))
+    return (osc * ExpDecay(0.3) / d).pan(pn)
 
 
-def part3():
-    pitch_spread = 0.1
-    def synth(p):
-        return psaw(p) *  ExpDecay(0.05)
-        #return psaw(p) *  LinearDecay(.8*step)
-
-    arp_idx_steps = [-1, -1, 1, 1, 1, 1]
-    def arp_idxs():
-        a = 2
-        while True:
-            for step in arp_idx_steps:
-                yield a
-                a += step
-
-    arp_pitch_lst = notes('F#3 G3 A3 C4 D4')
-    def arp_pitch(i):
-        octave = int(i / len(arp_pitch_lst))
-        idx = i % len(arp_pitch_lst)
-        return arp_pitch_lst[idx] + 12*octave
-
+step = 0.18
+def arp1(s, pan_spread=1):
+    ns = notes(s)
+    assert len(ns) == 4
+    pans = list(f_range(-pan_spread, pan_spread, len(ns)))
     c = Compose()
     delay = 0
-    for p in islice((arp_pitch(x) for x in arp_idxs()), 67):
-        c.add(synth(p), delay)
-        delay += step / 2
-
+    for x in [0,1,2,1,3,2,1]:
+        c.add(synth1(ns[x], pans[x], 1), delay)
+        delay += step
+    c.mlength = step * 7
     return c
 
-c1 = Compose()
-c1.add(3 * part1, 0)
-c1.add(5 * part2, 0)
+def arp1s(ss, *args, **kwargs):
+    return Chain(arp1(x, *args, **kwargs) for x in ss)
 
-c2 = Compose()
-c2.add(3 * (part1 + part1a), 0)
-c2.add(5 * part2, 0)
-c2.add(part3(), step*4*14)
-
-c = Compose()
-c.add(c1, 0)
-c.add(c2, step*4*14*2)
-
-a = c
-#a = part3()
-
-a *= .2
-
-a.play()
-
-mem_report()
+if __name__ == '__main__':
+    #synth1(note('C4'), 0).play()
+    a = arp1s(['E3 B3 D4 G4', 'D3 A3 C4 F4', 'G2 D3 F3 Bb3', 'A2 E3 G3 C4'])
+    #repeat(a, 8).audacity()
+    #b = arp1s(['B2 G3 B3 E4', 'A2 F3 A3 D4', 'D2 Bb2 D3 G3', 'E2 C3 E3 A2'])
+    #c = a + b
+    #c.mlength = a.mlength
+    c = arp1s(['Eb3 G3 Bb3 F4', 'F3 A3 C4 G4'])
+    #a.play()
+    (repeat(c, 1000)/2).play()
+    #(repeat(a, 4)/2).audacity()
