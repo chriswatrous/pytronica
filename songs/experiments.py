74c402666b253fd8b4738fb5f1f2078d52f3e1a3
Chris Watrous <chriswatrous@gmail.com>
2015-05-11 04:40:11 -0500

    bass drum experiment

--- songs/experiments.py
+++ songs/experiments.py
@@ -91,7 +91,7 @@ def vibrato():
     a *= .2
     a.play()
     #a.audacity()
-#vibrato()
+vibrato()
 
 def aliasing():
     c = Controller(5000)
@@ -122,4 +122,24 @@ def high_freq():
     c *= .5
     c.play()
     #c.audacity()
-high_freq()
+#high_freq()
+
+def bass_drum():
+    f = Controller(1000)
+    f.lineto(.01, 250)
+    f.lineto(.08, 40)
+    f.lineto(1, 40)
+
+    e = Controller(1)
+    e.lineto(.13, 1)
+    e.lineto(.18, 0)
+    a = e * Saw(f)
+
+    step = 60/120
+    c = Chain()
+    for _ in range(4):
+        c.add(a, step)
+    c = Chain([c]*16)
+    c.play()
+    #c.audacity()
+#bass_drum()
