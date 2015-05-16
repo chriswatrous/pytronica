afc131b06903ab7f2c3f0302526276402c261164
Chris Watrous <chriswatrous@gmail.com>
2015-05-16 03:23:27 -0500

    ambient experiment

--- songs/experiments.py
+++ songs/experiments.py
@@ -145,47 +145,115 @@ def bass_drum():
 #bass_drum()
 
 def multi_saw_experiment():
+    m = Sine(1.5)
     #osc = Saw
     #osc = multi_saw([1, 0], [-1, .50])
     #osc = multi_saw([1, 0], [-1, .25])
     #osc = multi_saw([1, 0], [-1, .25], [1, .55])
     #osc = multi_saw([1, 0], [-1, .25], [1, .50])
     #osc = multi_saw([1, 0], [-1, .25], [1, .55])
-    #osc = multi_saw([1, 0], [-1, .25], [1, .50], [1, .7])
+    #osc = multi_saw([1, 0], [-1, .25], [1, .50], [1, .7]) # good
     #osc = multi_saw([1, 0], [-1, .25], [1, .50], [1, .65]) # good
     #osc = multi_saw([1, 0], [-1, .25], [1, .50], [1, .6])
     #osc = multi_saw([1, 0], [-.5, .25], [1, .50], [1, .6])
     #osc = multi_saw([1.0, .00], [1.0, .30], [1.0, .70])
     #osc = multi_saw([1.0, .00], [1.0, .20], [1.0, .40])
-    osc = multi_saw([1.0, .00], [1.0, .25], [1.0, .50]) # good
+    osc = multi_saw([1.0, .00], [0.5, .125], [1.0, .25], [1.0, .50]) # good, full, bright
+    #osc = multi_saw([1.0, .00], [-0.5, .125], [1.0, .25], [1.0, .50])
+    #osc = multi_saw([1.0, .00], [1.0, .25], [1.0, .50]) # good, a little thin
+    #osc = multi_saw([1.0, .00], [-1.0, .18], [1.0, .50]) # good
+    #osc = multi_saw([1, 0], [-1, .375+.125*m])
+    #osc = multi_saw([.7, 0], [.7, .5+.50*Sine(1.5)], [.5, .5+.50*Sine(2.3)])
 
     def osc1(p):
         #return pitch_spread(p, osc, voices = 5, stereo_spread=1, random_phase=.5)
         #return pitch_spread(p, osc, voices = 4, spread=.1, stereo_spread=1, random_phase=1)
-        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=1)
+        return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=1)
         #return pitch_spread(p, osc, voices = 5, spread=.20, stereo_spread=1, random_phase=1)
-        return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=0.10)
-        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=0.05)
-        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=0.00)
+        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=0.10).drop(.10)
+        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=1, random_phase=0.05).drop(.1)
+        #return pitch_spread(p, osc, voices = 5, spread=.15, stereo_spread=-1, random_phase=0.00).drop(.1)
+
+    step = 2
 
     def chord(s):
         a = Layer()
         for p in notes(s):
             a.add(osc1(p))
-        return a
+        return a.take(step)
 
-    #(.05*chord('Gb3 Bb3 Db4 F4 Ab4')).playx()
-    step = 2
 
     a = Chain()
-    a.add(chord('Gb3 Bb3 Db4 F4 Ab4').take(step), step)
-    a.add(chord('Eb3 G3 Bb3 D4 F4').take(step), step)
-    a.add(chord('Db3 F3 Ab3 C4 Eb4').take(step), step)
-    a.add(chord('E3 Ab3 B3 Eb4 Gb4').take(step), step)
+    a.add(chord('Gb3 Bb3 Db4 F4 Ab4'), step)
+    a.add(chord('Eb3 G3 Bb3 D4 F4'), step)
+    a.add(chord('Db3 F3 Ab3 C4 Eb4'), step)
+    a.add(chord('E3 Ab3 B3 Eb4 Gb4'), step)
 
     a = Chain([a]*1000)
 
+    a *= .04
+    a.play()
+    #mem_report()
+    #a.take(10).audacity()
+    #print a.measure_rate()
+#multi_saw_experiment()
+
+
+def ambient1():
+    def osc1(p):
+        o = multi_saw([1.0, .00], [0.5, .125], [1.0, .25], [1.0, .50])
+        return pitch_spread(p, o, voices = 5, spread=.15, stereo_spread=1, random_phase=0).drop(.1)
+
+    step = 20
+
+    def chord(s):
+        a = Layer()
+        for p in notes(s):
+            a.add(osc1(p))
+        return a.take(step)
+
+    #m = (0.5 - 0.5 * Saw(20))
+    #m = (0.5 + 0.5 * Sine(20))
+    #m = (0.5 - 0.5 * Saw(20)) * (1 - 0.5 * (0.5 + 0.5 * Sine(0.3)))
+    #m = (0.6 - 0.4 * Saw(20 + 5*Sine(.3, .75))) * (0.8 + 0.2 * Sine(.5))
+
+    co = Controller(0)
+    co.lineto(120, 1)
+    co.lineto(240, 0)
+    co.lineto(10000, 0)
+
+    f1 = 4
+    f2 = 20
+    s = Saw((f1 + f2)/2 + ((f1 - f2)/2)*Sine(.12, .75))
+
+    b = .30
+    m = (1 - b) - b * s * co
+
+    c = Chain()
+    c.add(chord('A3 Bb3 D4 F4'), step)
+    c.add(chord('C4 D4 F4 A4'), step)
+    c.add(chord('B3 C4 E4 G4'), step)
+    c.add(chord('C4 E4 F4 A4'), step)
+
+    c = Chain([c]*1000)
+
+    def osc2(p):
+        o = multi_saw([1, 0], [-1, .25], [1, .50], [1, .7])
+        o = pitch_spread(p, o, voices = 5, spread=.15, stereo_spread=1, random_phase=1)
+        o += 0.5 * Sine(p2f(p))
+        return o
+
+    c2 = Chain()
+    c2.add(osc2(note('G1')).take(step), step)
+    c2.add(osc2(note('Bb1')).take(step), step)
+    c2.add(osc2(note('A1')).take(step), step)
+    c2.add(osc2(note('D2')).take(step), step)
+    c2 = Chain([c2]*1000)
+
+    a = m * c
+
+    a += c2
+
     a *= .05
     a.play()
-    #a.audacity()
-multi_saw_experiment()
+ambient1()
