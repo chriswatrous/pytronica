5c0b0fecd623f4357856924c0a7e46f7881525f8
Chris Watrous <chriswatrous@gmail.com>
2016-07-04 15:41:55 -0500

    Problem: import lines too long, not as clean as they could be

--- pytronica/__init__.py
+++ pytronica/__init__.py
@@ -1,18 +1,53 @@
 #! /usr/bin/python
-
 import pyximport
 pyximport.install()
 
-from pytronica.buffernode import mem_report, mem_report_clear
-from pytronica.generator import get_sample_rate, set_sample_rate, set_clip_reporting
-
-from pytronica.util import p2f, f2p, to_dB, from_dB, note, notes, note_freq, note_freqs, f_range
-from pytronica.synth import stereo_spread, psaw, repeat, psine, multi_saw, pitch_spread
-
+from pytronica.buffernode import (
+    mem_report,
+    mem_report_clear,
+)
+from pytronica.generator import (
+    get_sample_rate,
+    set_sample_rate,
+    set_clip_reporting,
+)
+from pytronica.util import (
+    f2p,
+    f_range,
+    from_dB,
+    note,
+    note_freq,
+    note_freqs,
+    notes,
+    p2f,
+    to_dB,
+)
+from pytronica.synth import (
+    multi_saw,
+    pitch_spread,
+    psaw,
+    psine,
+    repeat,
+    stereo_spread,
+)
 from pytronica.controller import Controller
 from pytronica.modifiers import Pan
 from pytronica.combiners import Layer
-from pytronica.envelopes import ExpDecay, LinearDecay
-from pytronica.compose import Compose, Chain
-from pytronica.misc import Silence, NoOp, Const
-from pytronica.osc import Saw, Sine, Noise
+from pytronica.envelopes import (
+    ExpDecay,
+    LinearDecay,
+)
+from pytronica.compose import (
+    Chain,
+    Compose,
+)
+from pytronica.misc import (
+    Const,
+    NoOp,
+    Silence,
+)
+from pytronica.osc import (
+    Noise,
+    Saw,
+    Sine,
+)
